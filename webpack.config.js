176edc784531a7741d7884e77fcfbb59bb604718
korla <johannes.bengtsson@raysearchlabs.com>
2016-04-16 17:24:23 +0200

    Init

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var webpack = require("webpack");
+var path = require("path");
+
+module.exports = {
+    entry: "./app.js",
+    output: {
+        path: __dirname,
+        filename: "./build/index.js"
+    },
+    resolve: {
+        root: [path.join(__dirname, "bower_components")]
+    },
+    plugins: [
+        new webpack.ResolverPlugin(
+            new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
+        )
+    ],
+    module: {
+        loaders: [
+            {
+              test: /\.less$/,
+              loader: "style!css!less"
+            },
+            {
+              test: /\.jsx?$/,
+              exclude: /(node_modules|bower_components)/,
+              loader: 'babel',
+              query: {
+                "presets": ["es2015"]
+              }
+            },
+            {
+              test: /\.vue$/,
+              loader: 'vue'
+            }
+        ]
+    },
+    babel: {
+      presets: ['es2015']
+    },
+    devtool: "#inline-source-map"
+};
