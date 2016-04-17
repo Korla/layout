f1e009bf01316cfa7fa453f0fe2f16fa1c2e91c1
korla <johannes.bengtsson@raysearchlabs.com>
2016-04-17 10:25:39 +0200

    rcHeader, rcPage, and rcContainer

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('Success');
+require('./app.less')
+angular.module('rc', [])
+  .controller('layoutController', ($scope) => {
+  })
+  .directive('rcHeader', () => ({
+    transclude: true,
+    replace: true,
+    template: `<div class="rc-header" ng-transclude></div>`
+  }))
+  .directive('rcPage', () => ({
+    transclude: true,
+    replace: true,
+    template: `<div class="rc-page" ng-transclude></div>`
+  }))
+  .directive('rcContainer', () => ({
+    transclude: true,
+    replace: true,
+    template: `<div class="rc-container" ng-transclude></div>`
+  }))
+;
