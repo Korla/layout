801aca342326ce3b8d131af154393f06dc078c28
korla <johannes.bengtsson@raysearchlabs.com>
2016-04-17 14:11:12 +0200

    tab panel

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ angular.module('rc', [])
     controller: () => {}
   }))
   .directive('rcPanel', () => ({
-    require: 'rcPage',
     transclude: {
       'header': '?rcPanelHeader',
       'body': 'rcPanelBody'
@@ -22,10 +21,46 @@ angular.module('rc', [])
     replace: true,
     template: `
       <div class="rc-panel">
-        <div ng-transclude="header" class="rc-panel-header">Fallback header</div>
-        <div ng-transclude="body" class="rc-panel-body"></div>
+        <div class="rc-panel-header" ng-transclude="header">Fallback header</div>
+        <div class="rc-panel-body" ng-transclude="body"></div>
       </div>
     `
+  }))
+  .directive('rcPanelTabs', () => ({
+    transclude: true,
+    scope: {},
+    replace: true,
+    template: `
+      <div class="rc-panel">
+        <div class="rc-panel-header">
+          <span class="rc-panel-header-tab"
+                ng-class="{'rc-panel-header-tab-selected': tab.selected}"
+                ng-repeat="tab in tabs"
+                ng-click="select(tab)"
+                ng-bind="tab.title"></span>
+        </div>
+        <div class="rc-panel-body" ng-transclude></div>
+      </div>
+    `,
+    controller: function($scope) {
+      $scope.tabs = [];
+      $scope.select = tabToSelect =>
+        $scope.tabs.forEach(tab => tab.selected = tab === tabToSelect);
+      this.addTab = tab => $scope.tabs.push(tab);
+    }
+  }))
+  .directive('rcPanelTab', () => ({
+    require: '^^rcPanelTabs',
+    transclude: true,
+    replace: true,
+    scope: {
+      title: '@',
+      selected: '@'
+    },
+    template: `<div class="rc-content rc-content-no-border"
+                    ng-show="selected"
+                    ng-transclude></div>`,
+    link: (scope, element, attrs, tabsController) => tabsController.addTab(scope)
   }))
   .directive('rcContent', () => ({
     transclude: true,
