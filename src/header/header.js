740755395be58b24c2dedd67190e01c6888743c2
korla <johannes.bengtsson@raysearchlabs.com>
2016-04-22 16:34:30 +0200

    Header

--- src/header/header.js
+++ src/header/header.js
@@ -1,7 +1,21 @@
 angular.module('rc')
   .directive('rcHeader', () => ({
-    transclude: true,
     replace: true,
-    template: `<div class="rc-header" ng-transclude></div>`
+    template: `
+    <div class="rc-header">
+      <span>
+        <span class="rc-header-logo">RayCare</span>
+        <span class="rc-header-tabs">
+          <span class="rc-header-tabs-selected">Today</span>
+          <span>Booking</span>
+          <span>Treatment</span>
+          <span>Patients</span>
+        </span>
+      </span>
+      <span class="rc-header-menu">
+        <span class="rc-header-menu-name">John Smith</span>
+        <span class="rc-header-menu-role">Booking admin</span>
+      </span>
+    </div>`
   }))
 ;
